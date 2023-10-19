86714f218505551fa6fe39b80dc2a6ff84f81279
Juan Jose <juanjosealifigueroa56@gmail.com>
2023-10-19 10:28:42 -0300

    agregamos un propmpt para pedir un color a un usario

--- js/ejercicio-1.js
+++ js/ejercicio-1.js
@@ -7,3 +7,18 @@ console.log("Suma = " + (numero1 + numero2));
 console.log("Resta = " + (numero1 - numero2));
 console.log("Division = " + (numero1 / numero2));
 console.log("MOD = " + (numero1 % numero2));
+
+
+//muestra el msj en el documento html
+document.write("<h1 class='tituloPrincipal'>Hello word <br> y se marchoo</h1>");
+
+//Ventaja emergente
+alert("Esto no es una simulación");     
+
+//solicitar un color al usuairo
+
+let color = prompt("ingrese su color favorito: ");
+console.log(color)
+
+color = "celeste";
+console.log(color)
