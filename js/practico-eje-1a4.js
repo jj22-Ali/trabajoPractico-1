fa9b0109d61ab882c7a54d9618c639c2f49963f9
Juan Jose <juanjosealifigueroa56@gmail.com>
2023-10-23 23:07:20 -0300

    corrigiendo detalles

--- js/practico-eje-1a4.js
+++ js/practico-eje-1a4.js
@@ -1,13 +1,13 @@
 // punto 1
-
+alert("Ejercicio 1")
 alert("Un mensaje")
 
 // punto 2 
-
+alert("Ejercicio 2")
 document.write("hello word");
 
 // punto 3
-
+alert("Ejercicio 3")
 document.write("<br>" + (3 + 5));
 
 // punto 4
